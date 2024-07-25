ca50bc608a0448454e94ede2f649ce976aa66453
Alexander Grimalovsky <alexander.grimalovsky@gmail.com>
2024-07-25 22:07:25 +0300

    Minor update

--- src/functions.php
+++ src/functions.php
@@ -4,15 +4,17 @@ declare(strict_types=1);
 
 namespace Flying\Util\Css;
 
-if (!\function_exists(classes::class)) {
-    /**
-     * Create string of CSS classes from provided classes information
-     *
-     * @param mixed ...$classes
-     * @return string
-     */
-    function classes(mixed ...$classes): string
-    {
-        return (string)Classes::from(...$classes);
-    }
+if (\function_exists(classes::class)) {
+    return;
+}
+
+/**
+ * Create a string of CSS classes from provided classes information
+ *
+ * @param mixed ...$classes
+ * @return string
+ */
+function classes(mixed ...$classes): string
+{
+    return (string)Classes::from(...$classes);
 }
